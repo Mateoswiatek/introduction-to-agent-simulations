7cef12ff088b759e740b693a1d6723ee8e770096
Mateusz Świątek <mswiatek2002@gmail.com>
2024-06-16 12:37:30 +0200

    group

--- MyModel.py
+++ MyModel.py
@@ -3,13 +3,17 @@ from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 
 
+
 class RandomWalker(Agent):
     """An agent that walks randomly."""
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, group):
         super().__init__(unique_id, model)
+        self.group = group
         self.x = self.random.randint(0, self.model.grid.width - 1)
         self.y = self.random.randint(0, self.model.grid.height - 1)
         self.model.grid.place_agent(self, (self.x, self.y))
+        self.previous_position = (self.x, self.y)
+        self.action_log = ""  # Dodatkowe pole do przechowywania informacji o działaniach agenta
 
     def step(self):
         """Move the agent to a random neighboring cell."""
@@ -18,8 +22,11 @@ class RandomWalker(Agent):
             moore=True,
             include_center=False
         )
+        self.previous_position = self.pos
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
+        self.action_log = f"Agent {self.unique_id} from group {self.group} moved from {self.previous_position} to {new_position}"
+
 
 class PopulationModel(Model):
     """A model with some number of agents."""
@@ -30,7 +37,8 @@ class PopulationModel(Model):
 
         # Create agents
         for i in range(self.num_agents):
-            a = RandomWalker(i, self)
+            group = 'A' if i < self.num_agents / 2 else 'B'
+            a = RandomWalker(i, self, group)
             self.schedule.add(a)
 
     def step(self):
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from MyModel import PopulationModel
-from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.modules import CanvasGrid, TextElement
 from mesa.visualization.ModularVisualization import ModularServer
 
 def agent_portrayal(agent):
@@ -8,11 +8,25 @@ def agent_portrayal(agent):
         "Color": "red",
         "Filled": "true",
         "Layer": 0,
-        "r": 0.5
+        "r": 0.5,
+        "text": str(agent.unique_id),
+        "text_color": "black"
     }
+
+    if agent.group == 'A':
+        portrayal["Color"] = "red"
+    else:
+        portrayal["Color"] = "blue"
     return portrayal
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+
+class ActionLogElement(TextElement):
+    def render(self, model):
+        log = [agent.action_log for agent in model.schedule.agents]
+        return "Actions:<br>" + "<br>".join(log)
+
+action_log_element = ActionLogElement()
 
 model_params = {
     "N": 10,  # Liczba agentów
@@ -22,7 +36,7 @@ model_params = {
 
 server = ModularServer(
     PopulationModel,
-    [grid],
+    [grid, action_log_element],
     "Random Walker Model",
     model_params
 )
